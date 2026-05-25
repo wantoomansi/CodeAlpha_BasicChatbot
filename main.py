867468353380b074b88d44179cf9f0e11bb0592e
Mansi Wantoo <wantoomansi@gmail.com>
2026-05-25 14:30:16 +0530

    First chatbot project

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Simple Chatbot")
+
+while True:
+    user = input("You: ").lower()
+
+    if user == "hello":
+        print("Bot: Hi!")
+    elif user == "how are you":
+        print("Bot: I am fine!")
+    elif user == "bye":
+        print("Bot: Goodbye!")
+        break
+    else:
+        print("Bot: I don't understand.")
